2b3a529b93d66fa636f16becab276bbaf19e6e39
Andrew Chok <chok.n.drew@gmail.com>
2022-12-09 09:57:19 -0500

    Added datasbase handling

--- bot.py
+++ bot.py
@@ -6,8 +6,11 @@ import random
 import datetime
 import discord
 from discord.ext import commands
-from discord.ext.commands import Context
+from discord.ext.commands import Context, Bot
 from dotenv import load_dotenv
+from getpass import getpass
+from mysql.connector import connect, Error
+import aiomysql
 
 #%% declaring intents
 intents = discord.Intents(
@@ -22,12 +25,128 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 DEVELOPER = os.getenv('DEV_ID')
 CMD_PREFIX = os.getenv('CMD_PREFIX')
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 
 #%% Utility Logic
 def FormatTimeToString(delta_time : datetime.timedelta):
     return str(int((delta_time.seconds % 3600) / 60)) + ' mins ' + str(delta_time.seconds % 60) + ' secs'
 
+def MentionAuthor(author):
+    authorId = str(author.id)
+    return '<@' + authorId + '>'
 
+#%% MySQL Logic
+class Database:
+    def field(command, *values):
+        try:
+            with connect(
+                host="localhost",
+                user=MYSQL_USERNAME,
+                password=MYSQL_PASSWORD,
+                database="leggo_dabloons_db"
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(command, tuple(values))
+                    fetch = cursor.fetchone()
+                    if fetch is not None:
+                        return fetch[0]
+                    return                    
+        except Error as e:
+            print("Error:")
+            print(e)
+
+    def record(command, *values):
+        try:
+            with connect(
+                host="localhost",
+                user=MYSQL_USERNAME,
+                password=MYSQL_PASSWORD,
+                database="leggo_dabloons_db"
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(command, tuple(values))
+                    return cursor.fetchone()                                    
+        except Error as e:
+            print("Error:")
+            print(e)
+    
+    def records(command, *values):
+        try:
+            with connect(
+                host="localhost",
+                user=MYSQL_USERNAME,
+                password=MYSQL_PASSWORD,
+                database="leggo_dabloons_db"
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(command, tuple(values))
+                    return cursor.fetchall()                                    
+        except Error as e:
+            print("Error:")
+            print(e)
+
+    def column(command, *values):
+        try:
+            with connect(
+                host="localhost",
+                user=MYSQL_USERNAME,
+                password=MYSQL_PASSWORD,
+                database="leggo_dabloons_db"
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(command, tuple(values))
+                    return [item[0] for item in cursor.fetchall()]                                   
+        except Error as e:
+            print("Error:")
+            print(e)
+
+    def execute(command, *values):
+        try:
+            with connect(
+                host="localhost",
+                user=MYSQL_USERNAME,
+                password=MYSQL_PASSWORD,
+                database="leggo_dabloons_db"
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(command, tuple(values))
+                    connection.commit()                                  
+        except Error as e:
+            print("Error:")
+            print(e)
+
+try:
+    with connect(
+        host="localhost",
+        user=MYSQL_USERNAME,
+        password=MYSQL_PASSWORD,
+        database="leggo_dabloons_db"
+    ) as connection:
+        print("Successful MySQL Connection Established!")
+        print(connection)
+
+        # insert_item_query = """
+        # INSERT INTO item (name, price, cooldown, stock_timer)
+        # VALUES ( %s, %s, %s, %s )
+        # """
+        # item_records= [
+        #     ("test1", 10, str(datetime.timedelta(minutes=5)), str(datetime.timedelta(minutes=8))),
+        #     ("test2", 10, str(datetime.timedelta(minutes=2)), str(datetime.timedelta(minutes=4))),
+        #     ("test3", 10, str(datetime.timedelta(minutes=3)), str(datetime.timedelta(minutes=2))),
+        #     ("test4", 10, str(datetime.timedelta(minutes=1)), str(datetime.timedelta(minutes=1)))
+        # ]
+        with connection.cursor() as cursor:
+            # cursor.executemany(insert_item_query, item_records)
+            cursor.execute("SELECT * FROM user")
+            for item in cursor.fetchall():
+                print(item)
+            connection.commit()
+            
+except Error as e:
+    print("Error:")
+    print(e)
+    
 #%% Class Logic
 class Item:
     # Constructor
@@ -79,10 +198,13 @@ class Store:
         )
         return self.closing_time - datetime.datetime.now()
 
+
 #%% Bot Logic
 temp_store = Store()
+encounter_cooldown = datetime.datetime.now()
+encounter_reset_time = datetime.timedelta(minutes=30)
 
-bot = commands.Bot(command_prefix=CMD_PREFIX, intents=intents)
+bot = Bot(command_prefix=CMD_PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
@@ -112,24 +234,61 @@ async def show_store(ctx: Context):
         f'{datetime.datetime.now()}:: show_store command triggerd {ctx.author}'
     )
 
+@bot.command(name='bag', help='')
+async def show_inventory(ctx: Context):
+    dabloons = Database.field("SELECT dabloons FROM user WHERE id = {}".format(ctx.author.id))
+    if not dabloons:
+        dabloons = 0
+    await ctx.send(MentionAuthor(ctx.author) + ' you have `{}` dabloons'.format(dabloons))
+    print(
+        f'{datetime.datetime.now()}:: show_store command triggerd {ctx.author}'
+    )
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
+    # if message was a command then do not trigger a chance for event
     if message.content.startswith(CMD_PREFIX):
         await bot.process_commands(message)
         return
 
-    encounter_chance = random.randint(1,1)
+    currentTime = datetime.datetime.now()
+    global encounter_cooldown
 
-    if(encounter_chance):
-        await message.channel.send('<:smilingcat:941188757376340029>: Hello Traveler! This is Dabloon Cat please take these `4` dabloons.')
-        
-        print(
-            f'{datetime.datetime.now()}:: {message.author} tiggered a dabloon gift event'
-        )
-    
+    if currentTime >= encounter_cooldown:
+        encounter_chance = random.randint(1,100)
+
+        if(encounter_chance == 1):        
+            user_exist = Database.record("SELECT 1 FROM user WHERE id = {}".format(message.author.id))
+            cat_emoji = '<:smilingcat:941188757376340029>'
+            if(user_exist):
+                dabloon_amount = random.randint(1,5)
+                await message.channel.send('''
+                    {}: Hello again Traveler {}!\n{}: Safe travels please take these `{}` dabloons.
+                    '''.format(cat_emoji, MentionAuthor(message.author), cat_emoji, dabloon_amount)
+                    )
+                Database.execute("UPDATE user SET dabloons = dabloons + {} WHERE id = {}".format(dabloon_amount, message.author.id))
+            else:
+                dabloon_amount = 4
+                await message.channel.send('''
+                    {}: Hello Traveler{}!\n{}: You have met Dabloon Cat please take these `{}` dabloons.
+                    '''.format(cat_emoji, MentionAuthor(message.author), cat_emoji, dabloon_amount)
+                    )
+                Database.execute("INSERT INTO user (id, name, dabloons) VALUES ({}, \"{}\", {})".format(message.author.id, message.author, dabloon_amount))
+            print(
+                f'{datetime.datetime.now()}:: {message.author} tiggered a dabloon gift event'
+            )
+            encounter_cooldown = datetime.datetime.now() + encounter_reset_time
+    #     else:
+    #         print(
+    #             f'{datetime.datetime.now()}:: encounter failed the roll'
+    #         )
+    # else:
+    #     print(
+    #             f'{datetime.datetime.now()}:: encounter on cooldown - {FormatTimeToString(encounter_cooldown-currentTime)}'
+    #         )
 
 # handle error for events
 @bot.event
